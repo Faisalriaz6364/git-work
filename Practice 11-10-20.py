cb0c67a72d8c7a310856d8558fbdfb3e433d7a89
Faisal Riaz <faisalriaz6364@gmail.com>
2020-10-28 22:25:56 +0500

    All things

---
+++ Practice 11-10-20.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Cotation mark single cotation ('') & dubual cotation mark ("")
+
+# # Text type / string
+
+# In[6]:
+
+
+print('hellow')
+
+
+# In[8]:
+
+
+print("helow")
+
+
+# # Assign string to a veriable (a is variable) & (text type is a string)
+
+# In[11]:
+
+
+a="hellow"
+print(a)
+
+
+# # Multiline string (Creating a multiline string)
+
+# In[21]:
+
+
+a="""I'm learning python
+I refer to teachbeamers.comtutorials.
+It is the most popular site for python programmers."""
+print(a)
+
+
+# # Numaric types
+# 
+# #int            Exp(x=1)
+# 
+# #float          Exp(2.7)
+# 
+# #complex        Exp(3j)
+
+# # Kis bhi (object) ki (type) malom karne k lia (type ka funtion) istamal kiya jata h
+
+# In[57]:
+
+
+type(3)
+
+
+# In[58]:
+
+
+type(2.3)
+
+
+# In[56]:
+
+
+type(3j)
+
+
+# # Squence types
+# USE
+# List funtion
+# 
+
+# In[60]:
+
+
+# 0 1 2 3 4 5  (Pasitive Index)
+# A B C D E F
+#-6-5-4-3-2-1  (Negative Index)
+
+
+# # List funtion
+
+# In[64]:
+
+
+thislist=["apple","banana","orange","cherry","kiwi"]
+print(thislist)
+
+
+# # Pasitive List range funtion
+
+# In[67]:
+
+
+print(thislist[1:4])
+
+
+# # Negative list range funtion
+
+# In[69]:
+
+
+print(thislist[-5])
+
